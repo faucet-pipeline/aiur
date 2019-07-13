d3b934395c55558a75afc007a44625a31ee12d2e
Steffen <mail@steffenhenschel.com>
2019-07-13 09:18:45 +0200

    Hacky styleguide style injection

--- example/aiur.config.js
+++ example/aiur.config.js
@@ -3,10 +3,17 @@
 exports.title = "Example Styleguide";
 exports.language = "en";
 
+// TODO is this a good idea?
+// exports.styles = [{
+// 	sass: "./components/index.scss"
+// }];
+
+// TODO configs for languages
+// exports.language = {
+// 	thymeleaf: {
 //
-exports.styles = {
-	sass: "./components/index.scss",
-}
+// 	}
+// }
 
 exports.pages = {
 	"": "./components/welcome.md",
--- example/faucet.config.js
+++
@@ -1,6 +0,0 @@
-// module.exports = {
-//   watchDirs: ["./src/ui"],
-//   sass: [{
-//     source: "./src/ui/styles/index.scss",
-//   }],
-// };
